14bc5c9a56f163967a664b2fbd050b50ab40ecbe
olikurt1 <olikurt1@outlook.com>
2026-01-06 12:08:32 +0000

    Updating task2 again

--- Week1/W1T2.java
+++ Week1/W1T2.java
@@ -1,7 +1,10 @@
 public class W1T2 {
     public static void main(String[] args){
         String name = "ollie";
-        int age = 19;
-        System.out.println(name + " is " + age + " years old.");
+        int A = 48;
+        int B = 40;
+        int C = 32;
+        int BBC = B*2 + C;
+        System.out.println("Alice A-Level grades are "+BBC+" UCAS points");
     }
 }
