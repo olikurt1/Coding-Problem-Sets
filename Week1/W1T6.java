a99b7c8be95502d7c51508912d0a9e0cea3d1df7
olikurt1 <olikurt1@outlook.com>
2026-01-06 12:41:53 +0000

    Completing task 6

---
+++ Week1/W1T6.java
@@ -0,0 +1,11 @@
+public class W1T6 {
+    public static void main(String[] args){
+        int years = 15;
+        int months = 8;
+
+        int retireAge = 67;
+        int retireyears = retireAge - years-1;
+        int retiremonths = 12 - months;
+        System.out.println("The student needs "+retireyears+ "years and "+retiremonths+" months more to retire.");
+    }
+}
