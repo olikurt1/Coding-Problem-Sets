344b74970c733afbe8f22053bc6861ed8cb1c79d
olikurt1 <olikurt1@outlook.com>
2026-01-06 14:24:05 +0000

    Completing week2 task 1

---
+++ Week2/W2T1.java
@@ -0,0 +1,17 @@
+
+
+package Week2;
+import java.util.Scanner;
+import java.lang.Math.*;
+
+public class W2T1 {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the radius of the circle in cm?");
+        double userRadius = input.nextDouble(); 
+        double area = userRadius * userRadius * Math.PI;
+        System.out.format("The area of the circle is %.2f cm^2. ", area);
+        
+
+    }
+}
