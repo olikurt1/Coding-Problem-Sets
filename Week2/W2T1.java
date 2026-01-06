7b919d77c9161ce4ed8d876b85b7c2d31c8ce313
olikurt1 <olikurt1@outlook.com>
2026-01-06 14:30:53 +0000

    Adding conditions to task 1 of week 2

--- Week2/W2T1.java
+++ Week2/W2T1.java
@@ -9,8 +9,14 @@ public class W2T1 {
         Scanner input = new Scanner(System.in);
         System.out.println("What is the radius of the circle in cm?");
         double userRadius = input.nextDouble(); 
-        double area = userRadius * userRadius * Math.PI;
-        System.out.format("The area of the circle is %.2f cm^2. ", area);
+        if(userRadius < 0){
+            System.out.println("The radius cannot be negative");
+        }
+        else{
+            double area = userRadius * userRadius * Math.PI;
+            System.out.format("The area of the circle is %.2f cm^2. ", area);
+        }
+        
         
 
     }
