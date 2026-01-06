bd01b927539d86c7efcbbd67039b9ff33424cf4a
olikurt1 <olikurt1@outlook.com>
2026-01-06 14:59:30 +0000

    Modifiying Task 2 for week 2

--- Week2/W2T2.java
+++ Week2/W2T2.java
@@ -1,10 +1,18 @@
 package Week2;
-
+import java.util.Scanner;
 public class W2T2 {
     public static void main(String[] args){
-        boolean weekday = true;
-        int time = 11;
-        if(weekday == true){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Is it a weekday? (y/n)");
+        String weekday = input.nextLine();
+        
+        System.out.println("What is the time?");
+        int time = input.nextInt();
+        if(time >24){
+            System.out.println("This time does not exist");
+        }
+
+        if(weekday == "y"){
             if(time < 12){
                 System.out.println("Go to your lecture.");
             }
@@ -15,13 +23,16 @@ public class W2T2 {
                 System.out.println("Time for bed");
             }
         }
-        else{
+        else if(weekday == "n"){
             if(time <= 17){
                 System.out.println("You can rest now");
             }
             else{
                 System.out.println("Time for bed.");
             }
+        }
+        else{
+            System.out.println("Incorrect selection");
         }
     }
 }
