1bd36cb6ea398c2f8af8e37d50da5057bd16e5ac
olikurt1 <olikurt1@outlook.com>
2026-01-07 13:24:17 +0000

    Completing task 6 mostly

---
+++ Week2/W2T6.java
@@ -0,0 +1,59 @@
+package Week2;
+import java.util.Scanner;
+public class W2T6 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("What day of christmas is it?");
+        int dayInput = input.nextInt();
+
+        switch(dayInput){
+            case 1:
+                System.out.println("On the 1st day of christmas,\n my true love sent to me\nA partridge in a pear tree");
+            break;
+
+            case 2:
+                System.out.println("On the 2nd day of christmas,\nmy true love sent to me\nA partridge in a pear tree");
+            break;
+
+            case 3:
+                System.out.println("On the 3rd day of christmas,\nmy true love sent to me\nA partridge in a pear tree");
+            break;
+
+            case 4:
+                System.out.println("On the 4th day of christmas,\nmy true love sent to me\nA partridge in a pear tree");
+            break;
+
+            case 5:
+                System.out.println("On the 5th day of christmas,\nmy true love sent to me\nA partridge in a pear tree");
+            break;
+
+            case 6:
+                System.out.println("On the 6th day of christmas,\nmy true love sent to me\nA partridge in a pear tree");
+            break;
+
+            case 7:
+                System.out.println("On the 7th day of christmas,\nmy true love sent to me\nA partridge in a pear tree");
+            break;
+
+            case 8:
+                System.out.println("On the 8th day of christmas,\nmy true love sent to me\nA partridge in a pear tree");
+            break;
+
+            case 9:
+                System.out.println("On the 9th day of christmas,\nmy true love sent to me\nA partridge in a pear tree");
+            break;
+
+            case 10:
+                System.out.println("On the 10th day of christmas,\nmy true love sent to me\nA partridge in a pear tree");
+            break;
+
+            case 11:
+                System.out.println("On the 11th day of christmas,\nmy true love sent to me\nA partridge in a pear tree");
+            break;
+
+            case 12:
+                System.out.println("On the 12th day of christmas,\nmy true love sent to me\nA partridge in a pear tree");
+            break;
+        }
+    }
+}
