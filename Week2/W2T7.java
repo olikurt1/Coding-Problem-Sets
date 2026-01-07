0f6cd4f29a3b4a3f6efcb794b4042d9917ef2aaa
olikurt1 <olikurt1@outlook.com>
2026-01-07 13:55:12 +0000

    Fixing task 7 week 2

--- Week2/W2T7.java
+++ Week2/W2T7.java
@@ -4,7 +4,8 @@ public class W2T7 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.println("Choose (r)ock, (p)aper, (s)cissors");
-        int weapon = input.nextLine().charAt(0) ?'r': "You chose rock. "? 'p':"You chose paper" ? 's':"you chose scissors";
+        int weapon = input.nextLine().charAt(0);
+        System.out.println(weapon == 'r'? "You chose rock.":weapon == 'p'?"You chose paper":weapon == 's'? "you chose scissors":"Cheater");
 
     }
 }
