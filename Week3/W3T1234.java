aa217e767937a5ab668230172d0dc6ca2b619927
olikurt1 <olikurt1@outlook.com>
2026-01-07 14:22:12 +0000

    Completing Task 1, 2, 3, 4 for week 3

---
+++ Week3/W3T1234.java
@@ -0,0 +1,29 @@
+package Week3;
+
+public class W3T1234 {
+    public static void main(String[] args){
+        
+        /*Task 1 */
+        int num = 1;
+        while(num < 11){
+            System.out.println(num);
+            num += 1;
+        }
+        /*Task 2 */
+        for(int x = 1;  x<11; x++){
+            System.out.println(x);
+        }
+        /*Task 3 */
+        for(int y = 1; y<18; y= y+ 4){
+            System.out.println(y);
+        }
+        /*Task 4 */
+        for(int z = 1; z<11; z++){
+            if(z%2 == 0){
+                System.out.println(z);
+            }
+        }
+
+
+    }
+}
