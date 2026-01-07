444f196f8b0b1716a9e346cb46dfc8d729ea22c9
olikurt1 <olikurt1@outlook.com>
2026-01-07 13:08:35 +0000

    Completing task 5 by making conditions more succinct

--- Week2/W2T5.java
+++ Week2/W2T5.java
@@ -5,10 +5,10 @@ public class W2T5 {
         boolean rainy = true;
         boolean weekday = false;
 
-        if(rainy == true){
+        if(rainy){
             System.out.println("You need an umbrella today");
         }
-        if(weekday == false){
+        if(weekday){
             System.out.println("You can rest today");
         }
     }
